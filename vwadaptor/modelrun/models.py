1a1b9bfceba2b757dc274619cba449056ad99052
Moinul Hossain <moinul.cse@gmail.com>
2015-09-23 19:56:00 -0700

    the repr function is refactored
    it didn't have the correct format

--- vwadaptor/modelrun/models.py
+++ vwadaptor/modelrun/models.py
@@ -13,7 +13,6 @@ from vwadaptor.database import (
 from vwadaptor.constants import PROGRESS_STATES
 from sqlalchemy.ext.hybrid import hybrid_property
 
-#from marshmallow import Schema, fields, pprint
 
 class ModelRun(SurrogatePK, Model):
 
@@ -22,7 +21,6 @@ class ModelRun(SurrogatePK, Model):
     title = Column(db.String(80), unique=True, nullable=False)
     model_name = Column(db.String(30), nullable=False)
     resources = relationship('ModelResource', backref='modelrun', lazy='dynamic')
-    #progress = relationship('ModelProgress', uselist=False,backref='progress')
     user_id = Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
     #'not_started','queued', 'running','finished','error'
     progress_state = Column( db.Enum(*tuple(PROGRESS_STATES.values())), default=PROGRESS_STATES['NOT_STARTED'])
@@ -31,7 +29,7 @@ class ModelRun(SurrogatePK, Model):
     def __init__(self,**kwargs):
         db.Model.__init__(self, **kwargs)
     def __repr__(self):
-        return '<ModelRun({id:name})>'.format(id=self.id,name=self.title)
+        return '<ModelRun({id}:{name})>'.format(id=self.id,name=self.title)
 
 
 class ModelResource(SurrogatePK, Model):
@@ -51,51 +49,3 @@ class ModelResource(SurrogatePK, Model):
 
     def __repr__(self):
         return '<ModelResource({type}--{name})>'.format(type=self.file_type,name=self.file_location)
-
-# class ModelResourceSchema(Schema):
-#     id = fields.Integer()
-#     resource_type = fields.String()
-#     resource_size = fields.Integer()
-#     resource_name = fields.String()
-#     modelrun_id = fields.Integer()
-    
-#     #def make_object(self, data):
-#     #    return ModelResource(**data)
-
-# class ModelRunSchema(Schema):
-#     id = fields.Integer()
-#     title = fields.String()
-#     model_name = fields.String()
-#     created_at = fields.DateTime()
-#     resources = fields.Nested(ModelResourceSchema,many=True)
-#     progress_state = fields.String()
-#     progress_value = fields.Float()
-#     user_id = fields.Integer()
-    
-    #def make_object(self, data):
-    #    return ModelRun(**data)
-
-
-# class ModelProgress(SurrogatePK, Model):
-#     __tablename__ = 'modelprogress'
-    
-#     state = Column('state', db.Enum('queued', 'running','finished','error'), default='queued')
-#     value = Column(db.Float(10), nullable=False, unique=True)
-#     model_run_id = Column(db.Integer, db.ForeignKey('modelruns.id'))
-
-#     def __init__(self, **kwargs):
-#         db.Model.__init__(self, **kwargs)
-
-#     def __repr__(self):
-#         return '<ModelResource({type}--{name})>'.format(type=self.file_type,name=self.file_location)
-
-
-
-# standard decorator style
-# @event.listens_for(ModelRun, 'after_insert')
-# def receive_after_insert(mapper, connection, modelrun):
-#     print connection
-#     progVals = {'value':0,'model_run_id':modelrun.id}
-#     progress = ModelProgress(**progVals)
-#     connection.add(progress)
-#     connection.commit()
