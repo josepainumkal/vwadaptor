ff82148dec5a089ac12d330539167a381eedf073
Moinul Hossain <moinul.cse@gmail.com>
2016-03-21 16:55:06 -0700

    updated jwt identity_handler
    user should not be allowed to access api until confirmed via email

--- vwadaptor/auth.py
+++ vwadaptor/auth.py
@@ -4,13 +4,15 @@ from flask.ext.security.utils import encrypt_password, verify_password
 from flask_jwt import JWT
 def authenticate(username, password):
     user = user_datastore.find_user(email=username)
-    if user and username == user.email and verify_password(password, user.password):
+    if user and user.confirmed_at and username == user.email and verify_password(password, user.password):
         return user
     return None
 
 
 def load_user(payload):
     user = user_datastore.find_user(id=payload['identity'])
-    return user
+    if user.confirmed_at:
+        return user
+    return None
 
 jwt = JWT(app=None,authentication_handler=authenticate, identity_handler=load_user)
